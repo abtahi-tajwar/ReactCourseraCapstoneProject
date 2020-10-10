f9dded408b55a9bb2cb3437124815f9e400adfb5
abtahi-tajwar <abtahitajwar@gmail.com>
2020-10-10 16:52:00 +0600

    Add files via upload

---
+++ precache-manifest.ad704f3c2f3b4e5b4bd88697d0ae92bb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dd10d6c78b291155652016d4e04b93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99557d008e03f71d6b63",
+    "url": "/static/css/2.f66d2689.chunk.css"
+  },
+  {
+    "revision": "68d142c6c556398086db",
+    "url": "/static/css/main.d6bcb662.chunk.css"
+  },
+  {
+    "revision": "99557d008e03f71d6b63",
+    "url": "/static/js/2.1cb81948.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1cb81948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d142c6c556398086db",
+    "url": "/static/js/main.32abe7a6.chunk.js"
+  },
+  {
+    "revision": "446036283c2a91d2c015",
+    "url": "/static/js/runtime-main.3f21e45c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
